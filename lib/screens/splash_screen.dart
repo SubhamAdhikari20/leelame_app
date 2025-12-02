e55bec50e9d775316a0cc28f55ae74b526ed1590
Subham Adhikari <subhamadhikari20@gmail.com>
2025-12-02 18:50:10 +0545

    Completion of Splash Screen Frontend

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,91 @@
+// lib/screens/splash_screen.dart
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:leelame/screens/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  Animation<double>? _scaleAnimation;
+  Animation<double>? _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1800),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.elasticOut),
+    );
+
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.3, 1.0),
+      ),
+    );
+
+    _animationController.forward();
+
+    Timer(Duration(milliseconds: 3000), () {
+      if (!mounted) {
+        return;
+      }
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: FadeTransition(
+          opacity: _opacityAnimation ?? AlwaysStoppedAnimation(1.0),
+          child: ScaleTransition(
+            scale: _scaleAnimation ?? AlwaysStoppedAnimation(1.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/leelame_logo_cropped_png.png",
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  height: MediaQuery.of(context).size.width * 0.3,
+                  fit: BoxFit.contain,
+                ),
+                SizedBox(height: 60),
+                CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    Colors.blueGrey,
+                  ),
+                  strokeWidth: 3,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
